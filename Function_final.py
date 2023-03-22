17893b7bb123c0b9b3f9273979a6764de026d53f
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2023-03-22 13:01:13 +0100

    Add final version of U-net semgentation and function

---
+++ Function_final.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 22 12:55:33 2023
+
+@author: nohel
+"""
+
---
+++ MAIN_segmentation_training.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 24 12:39:07 2022
+
+@author: nohel
+"""
