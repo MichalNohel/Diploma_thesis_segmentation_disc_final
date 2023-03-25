62828b23651ea0b731dcb744c2ac5d1e250f0f04
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2023-03-25 12:17:14 +0100

    přidání a trénování rozšířené databáze pro 25px

--- MAIN_segmentation_training.py
+++ MAIN_segmentation_training.py
@@ -43,13 +43,17 @@ if __name__ == "__main__":
     # size of crop image to training  
     output_size=(int(288),int(288),int(3))
     #Path to data resolution 25px
-    path_to_data="D:\Diploma_thesis_segmentation_disc_v2/Data_320_320_25px_preprocesing_all_database"
-    path_to_save_model = os.getcwd() + '/Trained_models/U_net_resolution_25px/'
+    
+    #path_to_data="D:\Diploma_thesis_segmentation_disc_v2/Data_320_320_25px_preprocesing_all_database"
+    #path_to_save_model = os.getcwd() + '/Trained_models/U_net_resolution_25px/'
+    
+    path_to_data="D:\Diploma_thesis_segmentation_disc_v2/Data_320_320_25px_preprocesing_all_database_doplneni"
+    path_to_save_model = os.getcwd() + '/Trained_models/U_net_resolution_25px_doplneni/'
     
     isExist = os.path.exists(path_to_save_model)
     if not isExist:
         os.makedirs(path_to_save_model)    
-    name_of_model='model_Unet_25px_all_mod_dat_OS_288_288_3'  
+    name_of_model='model_Unet_25px_all_mod_dat_doplneni_OS_288_288_3'  
     
     #%%
     
