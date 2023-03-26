37f846bdc9723faa225e2efff284c5c57ec75500
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2023-03-26 17:47:46 +0200

    Přidání vytvoření datasetu pro učení nnU-Netu v pythonu

---
+++ Preprocessing/MAIN_Part2_creation_of_train_and_test_dataset_for_nnU_net.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 26 17:00:35 2023
+
+@author: nohel
+"""
+
+from functions_MAIN_Part2 import maybe_mkdir_p,subfiles,convert_2d_image_to_nifti,generate_dataset_json
+import os
+join = os.path.join
+
+if __name__ == "__main__":
+        
+    base = 'D:\DATA\Data_25px_nn_unet'
+    
+    task_id = 700
+    task_name = "Optic_disc_cup_segmentation"
+    foldername = "Task%03.0d_%s" % (task_id, task_name)
+    out_base = join(base, foldername)  
+    
+    maybe_mkdir_p(out_base)    
+    target_imagesTr = join(out_base, "imagesTr")
+    target_imagesTs = join(out_base, "imagesTs")
+    target_labelsTr = join(out_base, "labelsTr")
+    target_labelsTs = join(out_base, "labelsTs")
+    
+    maybe_mkdir_p(target_imagesTr)
+    maybe_mkdir_p(target_labelsTs)
+    maybe_mkdir_p(target_imagesTs)
+    maybe_mkdir_p(target_labelsTr)
+
+    labels_dir_tr = join(base, 'training', 'output')
+    images_dir_tr = join(base, 'training', 'input')
+    training_cases = subfiles(labels_dir_tr, suffix='.png', join=False)    
+    
+    for t in training_cases:
+        unique_name = t[:-4]  # just the filename with the extension cropped away, so img-2.png becomes img-2 as unique_name
+        input_segmentation_file = join(labels_dir_tr, t)
+        input_image_file = join(images_dir_tr, t)
+
+        output_image_file = join(target_imagesTr, unique_name)  # do not specify a file ending! This will be done for you
+        output_seg_file = join(target_labelsTr, unique_name)  # do not specify a file ending! This will be done for you
+
+   
+        convert_2d_image_to_nifti(input_image_file, output_image_file, is_seg=False)
+        convert_2d_image_to_nifti(input_segmentation_file, output_seg_file, is_seg=True)        
+        
+    # now do the same for the test set
+    labels_dir_ts = join(base, 'testing', 'output')
+    images_dir_ts = join(base, 'testing', 'input')
+    testing_cases = subfiles(labels_dir_ts, suffix='.png', join=False)
+    for ts in testing_cases:
+        unique_name = ts[:-4]
+        input_segmentation_file = join(labels_dir_ts, ts)
+        input_image_file = join(images_dir_ts, ts)
+         
+        output_image_file = join(target_imagesTs, unique_name)
+        output_seg_file = join(target_labelsTs, unique_name)
+         
+        convert_2d_image_to_nifti(input_image_file, output_image_file, is_seg=False)
+        convert_2d_image_to_nifti(input_segmentation_file, output_seg_file, is_seg=True)
+   
+    #%%
+    # finally we can call the utility for generating a dataset.json
+    generate_dataset_json(join(out_base, 'dataset.json'), target_imagesTr, target_imagesTs, ('Red', 'Green', 'Blue'),
+                         labels={0: 'background', 1: 'disc',2: 'cup'}, dataset_name=task_name, license='hands off!')
+
+    
+    
+    
+    
---
+++ Preprocessing/functions_MAIN_Part2.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 26 17:38:29 2023
+
+@author: nohel
+"""
+
+#%%
+'Library import'
+from typing import List,Tuple
+from skimage import io
+import json
+import os
+import numpy as np
+import SimpleITK as sitk
+
+
+
+'Function definition'
+def maybe_mkdir_p(directory: str) -> None:
+    os.makedirs(directory, exist_ok=True)
+
+def subfiles(folder: str, join: bool = True, prefix: str = None, suffix: str = None, sort: bool = True) -> List[str]:
+    if join:
+        l = os.path.join
+    else:
+        l = lambda x, y: y
+    res = [l(folder, i) for i in os.listdir(folder) if os.path.isfile(os.path.join(folder, i))
+           and (prefix is None or i.startswith(prefix))
+           and (suffix is None or i.endswith(suffix))]
+    if sort:
+        res.sort()
+    return res     
+    
+def convert_2d_image_to_nifti(input_filename: str, output_filename_truncated: str, spacing=(999, 1, 1),
+                              transform=None, is_seg: bool = False) -> None:
+    """
+    Reads an image (must be a format that it recognized by skimage.io.imread) and converts it into a series of niftis.
+    The image can have an arbitrary number of input channels which will be exported separately (_0000.nii.gz,
+    _0001.nii.gz, etc for images and only .nii.gz for seg).
+    Spacing can be ignored most of the time.
+    !!!2D images are often natural images which do not have a voxel spacing that could be used for resampling. These images
+    must be resampled by you prior to converting them to nifti!!!
+
+    Datasets converted with this utility can only be used with the 2d U-Net configuration of nnU-Net
+
+    If Transform is not None it will be applied to the image after loading.
+
+    Segmentations will be converted to np.uint32!
+
+    :param is_seg:
+    :param transform:
+    :param input_filename:
+    :param output_filename_truncated: do not use a file ending for this one! Example: output_name='./converted/image1'. This
+    function will add the suffix (_0000) and file ending (.nii.gz) for you.
+    :param spacing:
+    :return:
+    """
+    img = io.imread(input_filename)
+
+    if transform is not None:
+        img = transform(img)
+
+    if len(img.shape) == 2:  # 2d image with no color channels
+        img = img[None, None]  # add dimensions
+    else:
+        assert len(img.shape) == 3, "image should be 3d with color channel last but has shape %s" % str(img.shape)
+        # we assume that the color channel is the last dimension. Transpose it to be in first
+        img = img.transpose((2, 1, 0))
+        # add third dimension
+        img = img[:, None]
+
+    # image is now (c, x, x, z) where x=1 since it's 2d
+    if is_seg:
+        assert img.shape[0] == 1, 'segmentations can only have one color channel, not sure what happened here'
+
+    for j, i in enumerate(img):
+
+        if is_seg:
+            i = i.astype(np.uint32)
+
+        itk_img = sitk.GetImageFromArray(i)
+        itk_img.SetSpacing(list(spacing)[::-1])
+        if not is_seg:
+            sitk.WriteImage(itk_img, output_filename_truncated + "_%04.0d.nii.gz" % j)
+        else:
+            sitk.WriteImage(itk_img, output_filename_truncated + ".nii.gz")
+            
+def get_identifiers_from_splitted_files(folder: str):
+    uniques = np.unique([i[:-12] for i in subfiles(folder, suffix='.nii.gz', join=False)])
+    return uniques
+
+def generate_dataset_json(output_file: str, imagesTr_dir: str, imagesTs_dir: str, modalities: Tuple,
+                          labels: dict, dataset_name: str, sort_keys=True, license: str = "hands off!", dataset_description: str = "",
+                          dataset_reference="", dataset_release='0.0'):
+    """
+    :param output_file: This needs to be the full path to the dataset.json you intend to write, so
+    output_file='DATASET_PATH/dataset.json' where the folder DATASET_PATH points to is the one with the
+    imagesTr and labelsTr subfolders
+    :param imagesTr_dir: path to the imagesTr folder of that dataset
+    :param imagesTs_dir: path to the imagesTs folder of that dataset. Can be None
+    :param modalities: tuple of strings with modality names. must be in the same order as the images (first entry
+    corresponds to _0000.nii.gz, etc). Example: ('T1', 'T2', 'FLAIR').
+    :param labels: dict with int->str (key->value) mapping the label IDs to label names. Note that 0 is always
+    supposed to be background! Example: {0: 'background', 1: 'edema', 2: 'enhancing tumor'}
+    :param dataset_name: The name of the dataset. Can be anything you want
+    :param sort_keys: In order to sort or not, the keys in dataset.json
+    :param license:
+    :param dataset_description:
+    :param dataset_reference: website of the dataset, if available
+    :param dataset_release:
+    :return:
+    """
+    train_identifiers = get_identifiers_from_splitted_files(imagesTr_dir)
+
+    if imagesTs_dir is not None:
+        test_identifiers = get_identifiers_from_splitted_files(imagesTs_dir)
+    else:
+        test_identifiers = []
+
+    json_dict = {}
+    json_dict['name'] = dataset_name
+    json_dict['description'] = dataset_description
+    json_dict['tensorImageSize'] = "4D"
+    json_dict['reference'] = dataset_reference
+    json_dict['licence'] = license
+    json_dict['release'] = dataset_release
+    json_dict['modality'] = {str(i): modalities[i] for i in range(len(modalities))}
+    json_dict['labels'] = {str(i): labels[i] for i in labels.keys()}
+
+    json_dict['numTraining'] = len(train_identifiers)
+    json_dict['numTest'] = len(test_identifiers)
+    json_dict['training'] = [
+        {'image': "./imagesTr/%s.nii.gz" % i, "label": "./labelsTr/%s.nii.gz" % i} for i
+        in
+        train_identifiers]
+    json_dict['test'] = ["./imagesTs/%s.nii.gz" % i for i in test_identifiers]
+
+    if not output_file.endswith("dataset.json"):
+        print("WARNING: output file name is not dataset.json! This may be intentional or not. You decide. "
+              "Proceeding anyways...")
+    save_json(json_dict, os.path.join(output_file), sort_keys=sort_keys)
+
+def save_json(obj, file: str, indent: int = 4, sort_keys: bool = True) -> None:
+    with open(file, 'w') as f:
+        json.dump(obj, f, sort_keys=sort_keys, indent=indent)
