109bb114d2e39b01900b0ed981ef59cde2eb28f8
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2023-03-26 16:59:27 +0200

    Přidání hlavních skriptů pro vytvoření databaze pro U-net a nnU-net

---
+++ Preprocessing/MAIN_creation_of_train_and_test_dataset_U_net.m
@@ -0,0 +1,92 @@
+close all
+clear all
+clc
+%%  SET
+path_to_data = 'D:\Preprocesing';
+path_to_data= [path_to_data '\'];
+
+%% Creation of test and train dataset
+output_image_size=[320,320]; %parametres for crop image
+percentage_number_test=0.2; % 20% of image will be in test dataset
+%parametres for OD detection
+sigma_detection=25;
+size_of_erosion=40;
+path_export_file='D:\DATA\Data_320_320_25px_preprocesing_sigma_50_all_database/';
+
+creation_of_train_and_test_dataset(path_to_data,output_image_size,sigma_detection,size_of_erosion,percentage_number_test,path_export_file)
+load chirp
+sound(y/10,Fs)
+
+%% Detection of centres in Test datasets
+test_images_file = dir([path_export_file 'Test\Images\*.png']);
+test_fov_file = dir([path_export_file 'Test\Fov\*.png']);
+test_dics_file = dir([path_export_file 'Test\Disc\*.png']);
+num_of_img=length(test_images_file);
+Disc_centres_test=[];
+Accuracy_of_detec=[];
+%% Detection with mistakes
+for i=1:num_of_img
+    image=imread([test_images_file(i).folder '\' test_images_file(i).name ]); 
+    fov=imread([test_fov_file(i).folder '\' test_fov_file(i).name ]);
+    mask_disc=logical(imread([test_dics_file(i).folder '\' test_dics_file(i).name ])); 
+    [center_new] = Detection_of_disc(image,fov,sigma_detection,size_of_erosion);
+    Disc_centres_test(i,1)=center_new(1);
+    Disc_centres_test(i,2)=center_new(2);
+    if mask_disc(center_new(2),center_new(1))==1
+        Accuracy_of_detec(i)=1;
+    else
+        Accuracy_of_detec(i)=0;
+    end
+end
+accuracy=sum(Accuracy_of_detec)/length(Accuracy_of_detec)
+%% save of test discs centers with mistakes
+Disc_centres_test=Disc_centres_test-1;
+save([path_export_file 'Disc_centres_test_with_mistakes.mat'],'Disc_centres_test')
+%% Detekce bez chyb
+for i=1:num_of_img
+    image=imread([test_images_file(i).folder '\' test_images_file(i).name ]); 
+    fov=imread([test_fov_file(i).folder '\' test_fov_file(i).name ]);
+    mask_disc=logical(imread([test_dics_file(i).folder '\' test_dics_file(i).name ])); 
+    [center_new] = Detection_of_disc(image,fov,sigma_detection,size_of_erosion);
+    Disc_centres_test(i,1)=center_new(1);
+    Disc_centres_test(i,2)=center_new(2);
+
+    if mask_disc(Disc_centres_test(i,2),Disc_centres_test(i,1))~=1
+        s = regionprops(mask_disc,'centroid');
+        Disc_centres_test(i,1)=round(s.Centroid(1));
+        Disc_centres_test(i,2)=round(s.Centroid(2));
+    end
+
+    if mask_disc(Disc_centres_test(i,2),Disc_centres_test(i,1))==1
+        Accuracy_of_detec(i)=1;
+    else
+        Accuracy_of_detec(i)=0;        
+    end
+end
+accuracy=sum(Accuracy_of_detec)/length(Accuracy_of_detec)
+
+%% save of test discs centers without mistakes
+Disc_centres_test=Disc_centres_test-1;
+save([path_export_file 'Disc_centres_test_correct.mat'],'Disc_centres_test')
+
+%% code for finding the optimal detection parameters
+% for m=1:length(sigma)
+%     for n=1:length(size_of_erosion)
+%         Disc_centres_test=[];
+%         Accuracy_of_detec=[];
+%         for i=1:num_of_img
+%             image=imread([test_images_file(i).folder '\' test_images_file(i).name ]); 
+%             fov=imread([test_fov_file(i).folder '\' test_fov_file(i).name ]);
+%             mask_disc=logical(imread([test_dics_file(i).folder '\' test_dics_file(i).name ])); 
+%             [center_new] = Detection_of_disc(image,fov,sigma(m),size_of_erosion(n));
+%             Disc_centres_test(i,1)=center_new(1);
+%             Disc_centres_test(i,2)=center_new(2);
+%             if mask_disc(center_new(2),center_new(1))==1
+%                 Accuracy_of_detec(i)=1;
+%             else
+%                 Accuracy_of_detec(i)=0;
+%             end
+%         end
+%         accuracy(m,n)=sum(Accuracy_of_detec)/length(Accuracy_of_detec)
+%     end
+% end
---
+++ Preprocessing/MAIN_creation_of_train_and_test_dataset_for_nnU_net.m
@@ -0,0 +1,163 @@
+close all
+clear all
+clc
+%%  SET
+path_to_data = 'D:\Preprocesing';
+path_to_data= [path_to_data '\'];
+
+%% Creation of test and train dataset for nnU-Net
+percentage_number_test=0.2; % 20% of image will be in test dataset
+path_export_file='D:\DATA\Data_25px_nn_unet\';
+
+% Creatio of file structure for creation of database
+if ~exist(path_export_file, 'dir')
+    mkdir(path_export_file)
+end
+if ~exist([path_export_file '\training\input'], 'dir')
+    mkdir([path_export_file '\training\input'])
+end
+if ~exist([path_export_file '\training\output'], 'dir')
+    mkdir([path_export_file '\training\output'])
+end
+if ~exist([path_export_file '\testing\input'], 'dir')
+    mkdir([path_export_file '\testing\input'])
+end
+if ~exist([path_export_file '\testing\output'], 'dir')
+    mkdir([path_export_file '\testing\output'])
+end
+
+
+
+%% Dristi-GS train  - Expert 1
+images_file = dir([path_to_data '\Drishti-GS\Images\*.png']);
+disc_file = dir([path_to_data '\Drishti-GS\Disc\expert1\*.png']);
+cup_file = dir([path_to_data '\Drishti-GS\Cup\expert1\*.png']);
+
+pom=52; % split to test and train dataset
+
+creation_of_nn_unet_dataset(images_file,disc_file,cup_file,pom,path_export_file)
+load chirp
+sound(y/10,Fs)
+%% REFUGE_train
+images_file = dir([path_to_data '\REFUGE\Images\Train\*.png']);
+disc_file = dir([path_to_data '\REFUGE\Disc\Train\*.png']);
+cup_file = dir([path_to_data '\REFUGE\Cup\Train\*.png']);
+
+pom=0; % split to test and train dataset
+creation_of_nn_unet_dataset(images_file,disc_file,cup_file,pom,path_export_file)
+load chirp
+sound(y/10,Fs)
+%% REFUGE_Validation
+images_file = dir([path_to_data '\REFUGE\Images\Validation\*.png']);
+disc_file = dir([path_to_data '\REFUGE\Disc\Validation\*.png']);
+cup_file = dir([path_to_data '\REFUGE\Cup\Validation\*.png']);
+
+pom=0; % split to test and train dataset
+creation_of_nn_unet_dataset(images_file,disc_file,cup_file,pom,path_export_file)
+load chirp
+sound(y/10,Fs)
+%% REFUGE_Test
+images_file = dir([path_to_data '\REFUGE\Images\Test\*.png']);
+disc_file = dir([path_to_data '\REFUGE\Disc\Test\*.png']);
+cup_file = dir([path_to_data '\REFUGE\Cup\Test\*.png']);
+
+pom=401; % split to test and train dataset
+creation_of_nn_unet_dataset(images_file,disc_file,cup_file,pom,path_export_file)
+load chirp
+sound(y/10,Fs)
+%% Riga - Bin Rushed
+images_file = dir([path_to_data '\RIGA\Images\BinRushed\*.png']);
+disc_file = dir([path_to_data '\RIGA\Disc\BinRushed\expert1\*.png']);
+cup_file = dir([path_to_data '\RIGA\Cup\BinRushed\expert1\*.png']);
+num_of_img=length(images_file);
+pom=round(num_of_img*percentage_number_test); % split to test and train dataset
+creation_of_nn_unet_dataset(images_file,disc_file,cup_file,pom,path_export_file)
+load chirp
+sound(y/10,Fs)
+
+%% Riga - Magrabia
+images_file = dir([path_to_data '\RIGA\Images\Magrabia\*.png']);
+disc_file = dir([path_to_data '\RIGA\Disc\Magrabia\expert1\*.png']);
+cup_file = dir([path_to_data '\RIGA\Cup\Magrabia\expert1\*.png']);
+num_of_img=length(images_file);
+pom=round(num_of_img*percentage_number_test); % split to test and train dataset
+creation_of_nn_unet_dataset(images_file,disc_file,cup_file,pom,path_export_file)
+load chirp
+sound(y/10,Fs)
+
+%% Riga - MESSIDOR
+images_file = dir([path_to_data '\RIGA\Images\MESSIDOR\*.png']);
+disc_file = dir([path_to_data '\RIGA\Disc\MESSIDOR\expert1\*.png']);
+cup_file = dir([path_to_data '\RIGA\Cup\MESSIDOR\expert1\*.png']);
+num_of_img=length(images_file);
+pom=round(num_of_img*percentage_number_test); % split to test and train dataset
+creation_of_nn_unet_dataset(images_file,disc_file,cup_file,pom,path_export_file)
+load chirp
+sound(y/10,Fs)
+%% RIM-ONE - Glaucoma
+images_file = dir([path_to_data '\RIM-ONE\Images\Glaucoma\*.png']);
+disc_file = dir([path_to_data '\RIM-ONE\Disc\Glaucoma\*.png']);
+cup_file = dir([path_to_data '\RIM-ONE\Cup\Glaucoma\*.png']);
+
+num_of_img=length(images_file);
+pom=round(num_of_img*percentage_number_test); % split to test and train dataset
+creation_of_nn_unet_dataset(images_file,disc_file,cup_file,pom,path_export_file)
+load chirp
+load chirp
+sound(y/10,Fs)
+
+%% RIM-ONE -  Healthy
+images_file = dir([path_to_data '\RIM-ONE\Images\Healthy\*.png']);
+disc_file = dir([path_to_data '\RIM-ONE\Disc\Healthy\*.png']);
+cup_file = dir([path_to_data '\RIM-ONE\Cup\Healthy\*.png']);
+
+num_of_img=length(images_file);
+pom=round(num_of_img*percentage_number_test); % split to test and train dataset
+creation_of_nn_unet_dataset(images_file,disc_file,cup_file,pom,path_export_file)
+load chirp
+load chirp
+sound(y/10,Fs)
+
+%% UoA_DR - Healthy
+images_file = dir([path_to_data '\UoA_DR\Images\Healthy\*.png']);
+disc_file = dir([path_to_data '\UoA_DR\Disc\Healthy\*.png']);
+cup_file = dir([path_to_data '\UoA_DR\Cup\Healthy\*.png']);
+
+num_of_img=length(images_file);
+pom=round(num_of_img*percentage_number_test); % split to test and train dataset
+creation_of_nn_unet_dataset(images_file,disc_file,cup_file,pom,path_export_file)
+load chirp
+sound(y/10,Fs)
+
+%% HRF - 
+images_file = dir([path_to_data '\HRF\Images\*.png']);
+disc_file = dir([path_to_data '\HRF\Disc\*.png']);
+cup_file = dir([path_to_data '\HRF\Cup\*.png']);
+num_of_img=length(images_file);
+pom=round(num_of_img*percentage_number_test); % split to test and train dataset
+creation_of_nn_unet_dataset(images_file,disc_file,cup_file,pom,path_export_file)
+load chirp
+sound(y/10,Fs)
+
+%%
+function []= creation_of_nn_unet_dataset(images_file,disc_file,cup_file,pom,path_export_file)
+    num_of_img=length(images_file);
+    for i=1:num_of_img
+        %expert 1
+        image=imread([images_file(i).folder '\' images_file(i).name ]); 
+        mask_disc=logical(imread([disc_file(i).folder '\' disc_file(i).name ]));  
+        mask_cup=logical(imread([cup_file(i).folder '\' cup_file(i).name ]));  
+
+        label=uint8(zeros(size(mask_disc)));
+        label(mask_disc)=1;
+        label(mask_cup)=2;
+                
+        if i>=pom
+            imwrite(image,[path_export_file 'training\input\' images_file(i).name])
+            imwrite(label,[path_export_file 'training\output\' images_file(i).name])            
+        else
+            imwrite(image,[path_export_file 'testing\input\' images_file(i).name])
+            imwrite(label,[path_export_file 'testing\output\' images_file(i).name]) 
+        end
+    end
+end
