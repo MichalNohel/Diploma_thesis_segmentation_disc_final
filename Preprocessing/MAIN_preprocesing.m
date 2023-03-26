5e224e996d6c068e4082874c8ea2a111e872d7ac
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2023-03-26 14:12:12 +0200

    přidání GT pozici disku v datech

--- Preprocessing/MAIN_preprocesing.m
+++ Preprocessing/MAIN_preprocesing.m
@@ -13,34 +13,33 @@ sigma=50;
 Num_tiles_param=150;
 ClipLimit=0.005;
 
+%% Preprocessing of databases and creation of GT disc positions
 %% Drishti-GS
 path=[path_to_data 'Drishti-GS\'];
 load_drishtigs(resolution, path, sigma, Num_tiles_param, ClipLimit );
-
+GT_disc_center_creation(path,'dristhi-gt')
 %% HRF
 path=[path_to_data 'HRF\'];
 load_hrf(resolution, path, sigma, Num_tiles_param, ClipLimit );
-
+GT_disc_center_creation(path,'HRF')
 %% REFUGE
 path=[path_to_data 'REFUGE\'];
 load_refuge(resolution, path, sigma, Num_tiles_param, ClipLimit );
-
+GT_disc_center_creation(path,'REFUGE')
 %% RIGA
 path=[path_to_data 'RIGA\'];
 load_riga_BinRushed(resolution, path, sigma, Num_tiles_param, ClipLimit );
 load_riga_Magrabia(resolution, path, sigma, Num_tiles_param, ClipLimit );
 load_riga_MESSIDOR(resolution, path, sigma, Num_tiles_param, ClipLimit );
-
+GT_disc_center_creation(path,'RIGA')
 %% RIM-ONE
 path=[path_to_data 'RIM-ONE\'];
 load_rimone(resolution, path, sigma, Num_tiles_param, ClipLimit );
-
+GT_disc_center_creation(path,'RIM-ONE')
 %% UoA_DR
 path=[path_to_data 'UoA_DR\'];
 load_uoadr(resolution, path, sigma, Num_tiles_param, ClipLimit );
+GT_disc_center_creation(path,'UoA_DR')
 load chirp
 sound(y/10,Fs)
-
-
-
 
